782299f215976cadde0170da9bfb62a0dc184525
Ryan Cotterell <rdc42@cst.cam.ac.uk>
2021-11-30 12:32:17 +0100

    added stubs

--- src/algorithms.py
+++ src/algorithms.py
@@ -8,7 +8,7 @@ def is_projective_naive(tree):
 	The naive algorithm works as follows.
 	For every head--dependency pair (h1, d1),
 	we check whether there exists another head--dependency 
-	pair (h2, d2) that interleaves. 
+	pair (h2, d2) that could interleave in a non-projective way. 
 	Since we conider all pairs, we only need to consider four cases: 
 	i)   h1, h2, d1, d2
 	ii)  h1, d2, d1, h2
@@ -28,15 +28,29 @@ def is_projective_naive(tree):
 	return True
 
 # projectivity test (linear algorithm)
+def is_projective(tree):
+	pass
 
 # pre-order collect (page 32)
+def pre_order_collect(tree):
+	pass
 
 # post-order collect (page 32)
+def post_order_collect(tree):
+	pass
 
 # treelet-order collect (page 34)
+def treelet_order_collect(tree):
+	pass
 
 # Extract-Order-Annotations (page 29)
+def extract_order_annotations(tree):
+	pass
 
 # tree to term (Chapter 3)
+def encode_proj(tree)
+	pass
 
 # term to tree (Chapter 3)
+def decode_proj(term):
+	pass
--- src/terms.py
+++ src/terms.py
@@ -1,3 +1,18 @@
+class OrderAnnotation(object):
+
+	def __init__(self, order):
+		self.order = order
+
+	def __str__(self):
+		return str(self.order)
+
+	def __repr__(self):
+		return repr(self.order)
+
+	def __getitem__(self, i):
+		return self.order[i]
+
+
 class Term(object):
 
 	def __init__(self, oa, lst):
@@ -5,3 +20,7 @@ class Term(object):
 		self.oa = oa
 		# list of the children
 		self.lst = lst
+
+	def __str__(self):
+		# TODO: visualization of the terms
+		return str(self)
