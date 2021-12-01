308c04cb16796b4897af43d07074b83d02f2e496
ferafda <94471363+ferafda@users.noreply.github.com>
2021-12-01 11:45:32 +0100

    doesn't work :(

--- src/algorithms.py
+++ src/algorithms.py
@@ -59,13 +59,17 @@ def post_order_collect(tree):
 
 def treelet_ordered_tree(tree):
 	t = {}
-	
-	for (i,j) in tree:
-		if i not in t.keys():
-			t[i] = [i]
-		t[i].append(j)
-	
+	for (i,j) in tree:	
+		if i in t.keys():
+			t[i].append(j)
+		else:
+			t[i] = [j]
+
 	for i in len(tree):
+		if i in t.keys():
+			t[i].append(i)
+		else:
+			t[i] = [i]
 		t[i] = sorted(t[i])
 	
 	return t
