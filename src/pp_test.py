6c9f6c7a3f0bb9de6e00325208881078e587fff6
tdelanversin <tdelanversin@gmail.com>
2021-12-01 10:40:51 +0100

    made unittests

--- src/pp_test.py
+++ src/pp_test.py
@@ -1,29 +1,27 @@
 from termdep import Tree
+import unittest
 
+class TestPrettyPrint(unittest.TestCase):
 
-def main():
-    """
-    example tree:
-    ((2, 0), (2, 1), (-1, 2), (5, 3), (5, 4), (2, 5), (2, 6))
-      -1
-       |
-       2 --\ 
-     / | \ |
-     0 1 5 6
-        / \ 
-        3 4
-    """
-    tree_list = ((2, 0), (2, 1), (-1, 2), (5, 3), (5, 4), (2, 5), (2, 6))
-    tree = Tree(tree_list, -1, "This is a text with words.")
-    size = tree.size()
-    depth = tree.depth()
-    matrix = tree.generate_matrix()
-    print("vertices: " + str(size))
-    print("depth:    " + str(depth))
-    print("text:     " + tree.text)
-    # print("matrix:   " + str(matrix))
-    print("matrix pp:\n" + Tree.string_of_matrix(matrix))
+    def test_easy_tree(self):
+      
+      ex_tree = ((2, 0), (2, 1), (-1, 2), (5, 3), (5, 4), (2, 5), (2, 6))
+      ex_sentence = "This is a text with words"
+      pp_string = """
+      ┏━┳━O━━━━━┳━┓\n
+      O O ┆ ┏━┳━O O\n
+      ┆ ┆ ┆ O O ┆ ┆\n
+      ┆ ┆ ┆ ┆ ┆ ┆ ┆\n
+      0 1 2 3 4 5 6\n
+      """
+      tree = Tree(ex_tree, -1, ex_sentence)
+      size = tree.size()
+      depth = tree.depth()
+      pp_matrix = Tree.string_of_matrix(tree.generate_matrix())
+      self.assertEqual(size, 7)
+      self.assertEqual(depth, 4)
+      self.assertEqual(ex_sentence, tree.text)
+      self.assertEqual(pp_string, pp_matrix)
 
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    unittest.main()
