94bce957e5c51dc72b44bffbb56d5f500806ac19
ferafda <rafael.david.fernandes0@gmail.com>
2021-12-02 15:30:08 +0100

    added some tests

--- src/algorithmsUnittests.py
+++ src/algorithmsUnittests.py
@@ -7,6 +7,10 @@ proj1 = ((-1, 0), (0, 4), (1, 3), (3, 2), (4, 1))
 proj2 = ((0, 1), (1, 2), (-1, 0))
 proj3 = ((0, 1), (1, 2), (-1, 3), (3, 0))
 proj4 = ((-1, 0), (5, 4), (1, 5), (0, 1), (2, 3), (4, 2))
+proj5 = ((2,1),(3,2),(-1,3),(3,9),(3,5),(5,4),(5,6),(6,8),(8,7),(1,0))
+proj6 = ((1,2),(2,3),(4,1),(5,4),(5,7),(8,5),(7,6),(1,0),(-1,8),(8,13),(13,9),(10,11),(13,12),(13,10))
+proj7 = ((2,1),(-1,2),(2,5),(2,3),(1,0),(5,4))
+proj8 = ((1,0),(-1,1),(1,3),(3,2),(1,4),(4,6),(6,5))
 
 # non-projective trees
 nonproj1 = ((1, 3), (2, 0), (0, 1), (-1, 2))
@@ -24,6 +28,10 @@ class TestProjectivity(unittest.TestCase):
         self.assertEqual(is_projective_naive(proj2), True)
         self.assertEqual(is_projective_naive(proj3), True)
         self.assertEqual(is_projective_naive(proj4), True)
+        self.assertEqual(is_projective_naive(proj5), True)
+        self.assertEqual(is_projective_naive(proj6), True)
+        self.assertEqual(is_projective_naive(proj7), True)
+        self.assertEqual(is_projective_naive(proj8), True)
 
     def test_non_projective_naive(self):
         self.assertEqual(is_projective_naive(nonproj1), False)
