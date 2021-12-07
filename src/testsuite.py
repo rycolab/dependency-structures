b94acf5f2644b7a85b4f1ced4e1d42a612c19cdd
tdelanversin <tdelanversin@gmail.com>
2021-12-07 16:56:28 +0100

    renaming

--- src/testsuite.py
+++ src/testsuite.py
@@ -1,9 +1,9 @@
 import unittest
 
 # import all unittest classes to run them all
-from prettyPrintUnittest import TestPrettyPrint
-from test_algorithms import TestAlgorithms, TestProjectivity
-from test_ryan import TestRyan
+from testPrettyPrint import TestPrettyPrint
+from testAlgorithm import TestAlgorithms, TestProjectivity
+from testRoundtrip import TestRyan
 
 if __name__ == '__main__':
     unittest.main()
