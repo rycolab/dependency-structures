ede9846dbceadda209a57d7bcdae99ace578d1c5
tdelanversin <tdelanversin@gmail.com>
2021-12-07 16:48:48 +0100

    updated testsuite and testryan

--- src/test_ryan.py
+++ src/test_ryan.py
@@ -1,3 +1,5 @@
+import unittest
+from unittest.main import main
 from algorithms import *
 
 # projective trees
@@ -7,9 +9,9 @@ proj.append(((0, 1), (1, 2), (-1, 0)))
 proj.append(((0, 1), (1, 2), (-1, 3), (3, 0)))
 proj.append(((-1, 0), (5, 4), (1, 5), (0, 1), (2, 3), (4, 2)))
 proj.append(((2, 1), (3, 2), (-1, 3), (3, 9), (3, 5),
-         (5, 4), (5, 6), (6, 8), (8, 7), (1, 0)))
+             (5, 4), (5, 6), (6, 8), (8, 7), (1, 0)))
 proj.append(((1, 2), (2, 3), (4, 1), (5, 4), (5, 7), (8, 5), (7, 6),
-         (1, 0), (-1, 8), (8, 13), (13, 9), (10, 11), (13, 12), (13, 10)))
+             (1, 0), (-1, 8), (8, 13), (13, 9), (10, 11), (13, 12), (13, 10)))
 proj.append(((2, 1), (-1, 2), (2, 5), (2, 3), (1, 0), (5, 4)))
 proj.append(((1, 0), (-1, 1), (1, 3), (3, 2), (1, 4), (4, 6), (6, 5)))
 
@@ -23,38 +25,41 @@ nonproj.append(((0, 2), (1, 0), (-1, 1)))
 nonproj.append(((0, 2), (3, 4), (-1, 0), (4, 1), (2, 3)))
 
 
-# helper functions
-def same(lst1, lst2):
-	set1, set2 = set(lst1), set(lst2)
-	for s in set1:
-		assert s in set2
-	for s in set2:
-		assert s in set1
-
-
-def diff(lst1, lst2):
-	def tmp(lst1, lst2):
+class TestRyan(unittest.TestCase):
+	# helper functions
+	def same(self, lst1, lst2):
 		set1, set2 = set(lst1), set(lst2)
 		for s in set1:
-			if s not in set2:
-				return True
+			self.assertIn(s, set2)
 		for s in set2:
-			if s in set1:
-				return True
-		return False
-	assert tmp(lst1, lst2)
+			self.assertIn(s, set1)
 
 
-# projective unit test
-for tree1 in proj:
-	term = encode_proj(tree1)
-	tree2 = decode_proj(term)
-	same(tree1, tree2)
+	def diff(self, lst1, lst2):
+		def tmp(lst1, lst2):
+			set1, set2 = set(lst1), set(lst2)
+			for s in set1:
+				if s not in set2:
+					return True
+			for s in set2:
+				if s in set1:
+					return True
+			return False
+		self.assertTrue(tmp(lst1, lst2))
 
-# non-projective unit test
-for tree1 in nonproj:
-	term = encode_proj(tree1)
-	tree2 = decode_proj(term)
-	diff(tree1, tree2)
+	def test_proj(self):
+		# projective unit test
+		for tree1 in proj:
+			term = encode_proj(tree1)
+			tree2 = decode_proj(term)
+			self.same(tree1, tree2)
 
+	def test_non_proj(self):
+		# non-projective unit test
+		for tree1 in nonproj:
+			term = encode_proj(tree1)
+			tree2 = decode_proj(term)
+			self.diff(tree1, tree2)
 
+if __name__ == '__main__':
+    unittest.main()
