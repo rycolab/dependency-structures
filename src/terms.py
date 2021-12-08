894c249dc880514a1263a73655f3a9c0bcb92b36
Federico Mantovani <fettemanto@gmail.com>
2021-12-08 10:50:16 +0100

    Improved term string representation, easier term construction

--- src/terms.py
+++ src/terms.py
@@ -24,7 +24,7 @@ class OrderAnnotation(object):
 
     @staticmethod
     def degK(*orders: List[int]) -> OrderAnnotation:
-        """Create an order annotation of arbitrary block-degree. Orders are passed as lists of 
+        """Create an order annotation of arbitrary block-degree. Orders are passed as lists of
         orders for each block.
 
         Returns:
@@ -57,18 +57,22 @@ class OrderAnnotation(object):
 class Term(object):
 
     @staticmethod
-    def inner(oa: OrderAnnotation, lst: List[Term]):
+    def inner(oaList: list, lst: List[Term]) -> Term:
+        oa = None
+        if all([isinstance(x, list) for x in oaList]):
+            # Block-degree k
+            oa = OrderAnnotation(oaList)
+        elif all([isinstance(x, int) for x in oaList]):
+            # Block-degree 1
+            oa = OrderAnnotation([oaList])
+        else:
+            raise TypeError(
+                'Invalid type for order annotation list: ' + str(type(oaList)))
         return Term(oa, lst)
 
     @staticmethod
-    def leaf(oa: OrderAnnotation):
-        return Term(oa, [])
-
-    @staticmethod
-    def fromTuples(tup: Tuple[OrderAnnotation, list]):
-        node = tup[0]
-        children = map(Term.fromTuples, tup[1])
-        return Term(node, list(children))
+    def leaf() -> Term:
+        return Term(OrderAnnotation.leaf(), [])
 
     def __init__(self, oa: OrderAnnotation, lst: List[Term]):
         # an order annotation serves as the functor of the term
@@ -76,13 +80,32 @@ class Term(object):
         # list of the children
         self.lst = lst
 
-    def __str__(self):
-        def aux(term: Term, level: int):
-            return '│ ' * (level - 1) + \
-                ('├─' if level > 0 else '') + str(term.oa) + \
-                '\n'.join([''] + [aux(x, level + 1) for x in term.lst])
+    def toString1(self):
+        """Converts the term to a string representing a tree similar to bash's `tree` command
+        """
+        def aux(term: Term, level: int, lastSibling: bool):
+            nodeStr = str(term.oa)
+            childrenStr = ''
+            for i, child in enumerate(term.lst):
+                last = i == len(term.lst) - 1
+                prefix = ('    ' if lastSibling else '│   ') * level
+                connector = '└── ' if last else '├── '
+                childrenStr += '\n' + prefix + \
+                    connector + aux(child, level + 1, last)
+            return nodeStr + childrenStr
+        return aux(self, 0, True)
 
-        return aux(self, 0)
+    def toString2(self):
+        """Converts the term to a string representing a tree similar to Kuhlmann's representation in
+        his book
+        """
+        pass
+
+    def __str__(self):
+        return self.toString1()
+
+    def __repr__(self) -> str:
+        return '\n' * 2 + str(self)
 
     def __len__(self):
         return len(self.lst)
