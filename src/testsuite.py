ab104cc5624b53b77ec22768a8508b2437ef26fa
tdelanversin <tdelanversin@gmail.com>
2021-12-09 15:11:06 +0100

    combining tests

--- src/testsuite.py
+++ src/testsuite.py
@@ -1,10 +1,9 @@
 import unittest
 
 # import all unittest classes to run them all
-from testPrettyPrint import TestPrettyPrint
+from testPrettyPrint import TestPrettyPrint, TestTermPrint
 from testAlgorithm import TestProjectivity, TestAlgorithms
 from testRoundtrip import TestRoundTrip
-from testTermPrint import TestTermPrint
 
 if __name__ == '__main__':
     unittest.main()
